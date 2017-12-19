8a0673a22a2dbd118b47219b3aa587e3805a088c
abhattacherjee <abhishek@bhattacherjee.com>
2017-12-19 13:43:04 -0700

    User mapping and Exception mapping

--- src/main/java/com/bhattacherjee/app/delegation/db/mappers/UserMapper.java
+++ src/main/java/com/bhattacherjee/app/delegation/db/mappers/UserMapper.java
@@ -1,4 +1,17 @@
 package com.bhattacherjee.app.delegation.db.mappers;
 
-public class UserMapper {
+import com.bhattacherjee.app.delegation.api.User;
+import org.skife.jdbi.v2.StatementContext;
+import org.skife.jdbi.v2.tweak.ResultSetMapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class UserMapper implements ResultSetMapper<User> {
+
+
+    @Override
+    public User map(int i, ResultSet resultSet, StatementContext statementContext) throws SQLException {
+        return new User(resultSet.getString("email"));
+    }
 }
